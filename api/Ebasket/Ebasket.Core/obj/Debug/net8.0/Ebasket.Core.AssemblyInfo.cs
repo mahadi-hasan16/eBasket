ba0b459a4b484f9b0e35de062d0195c4cc431e67
mahadi-hasan16 <mahadi.hasan.khan16@gmail.com>
2025-07-12 00:49:24 +0600

    Added the changes after installing Windows

--- api/Ebasket/Ebasket.Core/obj/Debug/net8.0/Ebasket.Core.AssemblyInfo.cs
+++ api/Ebasket/Ebasket.Core/obj/Debug/net8.0/Ebasket.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ebasket.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f37335ec962c34e7aabb474a337a98e4751de67f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022ae289a18482dceb761e45e40df0e43a4c4a81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ebasket.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ebasket.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- api/Ebasket/Ebasket.Infrastructure/obj/Debug/net8.0/Ebasket.Infrastructure.AssemblyInfo.cs
+++ api/Ebasket/Ebasket.Infrastructure/obj/Debug/net8.0/Ebasket.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ebasket.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1059a0a1e068cea4f926a0529165e6f50c7a765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022ae289a18482dceb761e45e40df0e43a4c4a81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ebasket.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ebasket.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
